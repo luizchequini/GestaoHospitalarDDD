8f4a97152c7fbc27376883be279995dd4afff97e
Luiz Chequini <luizchequini@hotmail.com>
2020-10-20 21:26:32 -0300

    8: Models, ORM CodeFirst e Migrations

---
+++ GestaoHospitalarDDD/Data_GestaoHospitalar/ORM/GestaoHospitalarDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data_GestaoHospitalar.ORM
+{
+    public class GestaoHospitalarDbContext : DbContext
+    {
+        public GestaoHospitalarDbContext(DbContextOptions<GestaoHospitalarDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
