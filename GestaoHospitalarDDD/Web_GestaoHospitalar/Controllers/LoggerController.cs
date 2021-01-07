959e3a9640916dcd5d2246ac22ef0e4709d2fe58
Luiz Chequini <luizchequini@gmail.com>
2021-01-06 23:54:32 -0300

    Seção 16:Auditoria com ILogger 77. Fazendo log de tudo no AspNet Core - Introdução

---
+++ GestaoHospitalarDDD/Web_GestaoHospitalar/Controllers/LoggerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Web_GestaoHospitalar.Controllers
+{
+    public class LoggerController : Controller
+    {
+        private readonly ILogger<LoggerController> _logger;
+
+        public LoggerController(ILogger<LoggerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var msgLogger = "TESTANDO EXIBIÇÃO DE LOGGER";
+
+            _logger.Log(LogLevel.Critical, msgLogger);
+            _logger.Log(LogLevel.Warning, msgLogger);
+            _logger.Log(LogLevel.Trace, msgLogger);
+            _logger.LogError(msgLogger);
+
+            try
+            {
+                throw new Exception("UMA EXCEÇÃO GERADA PARA DE TESTE DE AUDITORIA");
+            }
+            catch (Exception e)
+            {
+
+                _logger.LogError(e.Message);
+            }
+
+            ViewData["msgLogger"] = msgLogger;
+
+            return View();
+        }
+    }
+}
