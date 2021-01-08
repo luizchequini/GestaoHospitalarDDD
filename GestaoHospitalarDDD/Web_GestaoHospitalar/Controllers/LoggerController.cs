577e5451fd43adbf65d8154168526ab98ba87797
Luiz Chequini <luizchequini@gmail.com>
2021-01-07 21:57:00 -0300

    Seção 16:Auditoria com ILogger 82. Logando com ferramentas de terceiros - Parte 4

--- GestaoHospitalarDDD/Web_GestaoHospitalar/Controllers/LoggerController.cs
+++ GestaoHospitalarDDD/Web_GestaoHospitalar/Controllers/LoggerController.cs
@@ -20,13 +20,22 @@ namespace Web_GestaoHospitalar.Controllers
         {
             var msgLogger = "ATENÇÃO: UM ERRO PROPOSITAL OCORREU.\nContate o administrador!";
 
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                _logger.Trace($"O usuario {HttpContext.User.Identity.Name} realizou este procedimento");
+            }
+            else
+            {
+                _logger.Trace("Usuário sem login realizou este procedimento de erro.");
+            }
+
             try
             {
                 throw new Exception(msgLogger);
             }
             catch (Exception e)
             {
-                _logger.Error(e);
+                _logger.Error($"{e} = {HttpContext.User.Identity.Name}");
             }
 
             _logger.Debug("Hello world from .NET Core 3.x!");
--- GestaoHospitalarDDD/Web_GestaoHospitalar/Startup.cs
+++ GestaoHospitalarDDD/Web_GestaoHospitalar/Startup.cs
@@ -69,7 +69,8 @@ namespace Web_GestaoHospitalar
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                //app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Logger/Index");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
