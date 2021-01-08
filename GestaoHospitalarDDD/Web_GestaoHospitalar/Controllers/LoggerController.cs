379a8de102ae3cc237d7540f8c673acfaf178a69
Luiz Chequini <luizchequini@gmail.com>
2021-01-08 19:43:42 -0300

    Seção 16:Auditoria com ILogger 83. Logando com ferramentas de terceiros - Parte 5 Neste commit testei algumas funcionalidades do Kisslog, forçando no código Asp.Net Core alguns erros sem tratamento para que no Dashboard do Kisslog mostre os erros correspondentes, assim fechando a seção 16.

--- GestaoHospitalarDDD/Web_GestaoHospitalar/Controllers/LoggerController.cs
+++ GestaoHospitalarDDD/Web_GestaoHospitalar/Controllers/LoggerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using KissLog;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Web_GestaoHospitalar.Controllers
@@ -16,29 +17,26 @@ namespace Web_GestaoHospitalar.Controllers
             _logger = logger;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
-            var msgLogger = "ATENÇÃO: UM ERRO PROPOSITAL OCORREU.\nContate o administrador!";
+            var usuario = HttpContext.User.Identity.Name;
+            //var msgLogger = $"Exception try: {usuario}: UM ERRO PROPOSITAL OCORREU.\nContate o administrador!";
 
-            if (HttpContext.User.Identity.IsAuthenticated)
-            {
-                _logger.Trace($"O usuario {HttpContext.User.Identity.Name} realizou este procedimento");
-            }
-            else
-            {
-                _logger.Trace("Usuário sem login realizou este procedimento de erro.");
-            }
+            //_logger.Trace($"O usuario {usuario} realizou este procedimento");
 
-            try
-            {
-                throw new Exception(msgLogger);
-            }
-            catch (Exception e)
-            {
-                _logger.Error($"{e} = {HttpContext.User.Identity.Name}");
-            }
+            //try
+            //{
+            //    throw new Exception(msgLogger);
+            //}
+            //catch (Exception e)
+            //{
+            //    _logger.Error($"{e} = Exception catch: {usuario}");
+            //}
 
-            _logger.Debug("Hello world from .NET Core 3.x!");
+            //_logger.Debug("Hello world from .NET Core 3.x!");
+
+            throw new Exception($"FORÇANDO ERRO SEM TRATAMENTO: {usuario}!");
 
             return View();
         }
